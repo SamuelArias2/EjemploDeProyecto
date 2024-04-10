3458ac33d1d2ffaa773f9b4867a15ac969454c07
SamuelArias2 <166361317+SamuelArias2@users.noreply.github.com>
2024-04-10 11:08:43 -0600

    Tercer Commit

---
+++ SysEducaConnect.DAL/BDContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SysEducaConnect.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysEducaConnect.DAL
+{
+    public class BDContext:DbContext
+    {
+        public DbSet<Rol> Rol { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-DEV01\SQLEXPRESS02;Initial Catalog=SeguridadWebdb;Integrated Security=True");
+        }
+    }
+}
