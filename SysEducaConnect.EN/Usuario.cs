ef476220797282acef4a9c1487f22986dcfd4d29
SamuelArias2 <166361317+SamuelArias2@users.noreply.github.com>
2024-04-10 11:09:17 -0600

    Cuarto Commit

---
+++ SysEducaConnect.EN/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysEducaConnect.EN
+{
+    internal class Usuario
+    {
+    }
+}
