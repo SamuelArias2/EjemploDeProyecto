9bbca7655a029f593bb1641bed2250102e74ddcc
SamuelArias2 <166361317+SamuelArias2@users.noreply.github.com>
2024-04-12 09:40:02 -0600

    Add files via upload

---
+++ SysEducaConnect.UI/Controllers/HomePageController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using SysEducaConnect.EN;
+using SysEducaConnect.DAL;
+using System.Security.Claims;
+
+namespace SysEducaConnect.UI.Controllers
+{
+    public class HomePageController : Controller
+    {
+        public IActionResult PaginaInicio()
+        {
+            return View();
+        }
+
+
+    }
+}
